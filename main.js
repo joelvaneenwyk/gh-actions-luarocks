58c2cdfc90ee0cb250766744115317fab0c5708a
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-08 20:10:49 -0700

    Switch to using ESBuild to generate standalone script (#7)
    - Upgrade action to use 'node20'
    - Add 'ESBuild' and use it to transform the script to a standalone/bundled file in 'dist/main.js'

--- main.js
+++ main.js
@@ -1,101 +1,106 @@
+/**
+ * This script installs LuaRocks on the runner.
+ *
+ * cspell:ignore luarocks, cpath
+ */
 
-const core = require("@actions/core")
-const exec = require("@actions/exec")
-const io = require("@actions/io")
-const tc = require("@actions/tool-cache")
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const io = require("@actions/io");
+const tc = require("@actions/tool-cache");
 
-const path = require("path")
+const path = require("path");
 
-const BUILD_PREFIX = ".build-luarocks"
+const BUILD_PREFIX = ".build-luarocks";
 
-const LUA_PREFIX = ".lua" // default location for existing Lua installation
-const LUAROCKS_PREFIX = ".luarocks" // default location for LuaRocks installation
+const LUA_PREFIX = ".lua"; // default location for existing Lua installation
+const LUAROCKS_PREFIX = ".luarocks"; // default location for LuaRocks installation
 
 async function main() {
-  const luaRocksVersion = core.getInput('luaRocksVersion', { required: true })
+  const luaRocksVersion = core.getInput('luaRocksVersion', { required: true });
 
-  const luaRocksExtractPath = path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX, `luarocks-${luaRocksVersion}`)
-  const luaRocksInstallPath = path.join(process.cwd(), LUAROCKS_PREFIX)
+  const luaRocksExtractPath = path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX, `luarocks-${luaRocksVersion}`);
+  const luaRocksInstallPath = path.join(process.cwd(), LUAROCKS_PREFIX);
 
-  const sourceTar = await tc.downloadTool(`https://luarocks.org/releases/luarocks-${luaRocksVersion}.tar.gz`)
-  await io.mkdirP(luaRocksExtractPath)
-  await tc.extractTar(sourceTar, path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX))
+  const sourceTar = await tc.downloadTool(`https://luarocks.org/releases/luarocks-${luaRocksVersion}.tar.gz`);
+  await io.mkdirP(luaRocksExtractPath);
+  await tc.extractTar(sourceTar, path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX));
 
-  const configureArgs = []
+  const configureArgs = [];
   if (core.getInput("withLuaPath")) {
-    configureArgs.push(`--with-lua="${core.getInput("withLuaPath")}"`)
+    configureArgs.push(`--with-lua="${core.getInput("withLuaPath")}"`);
   } else {
     // NOTE: this is the default install path provided by gh-actions-lua
-    const luaInstallPath = path.join(process.cwd(), LUA_PREFIX)
-    configureArgs.push(`--with-lua="${luaInstallPath}"`)
+    const luaInstallPath = path.join(process.cwd(), LUA_PREFIX);
+    configureArgs.push(`--with-lua="${luaInstallPath}"`);
   }
 
-  configureArgs.push(`--prefix="${luaRocksInstallPath}"`)
+  configureArgs.push(`--prefix="${luaRocksInstallPath}"`);
 
   await exec.exec(`./configure ${configureArgs.join(" ")}`, undefined, {
     cwd: luaRocksExtractPath
-  })
+  });
 
   await exec.exec("make", undefined, {
     cwd: luaRocksExtractPath
-  })
+  });
 
   // NOTE: make build step is only necessary for luarocks 2.x
   if (luaRocksVersion.match(/^2\./)) {
     await exec.exec("make build", undefined, {
       cwd: luaRocksExtractPath
-    })
+    });
   }
 
   await exec.exec("make install", undefined, {
     cwd: luaRocksExtractPath
-  })
+  });
 
   // Update environment to use luarocks directly
-  let lrPath = ""
+  let lrPath = "";
 
   await exec.exec(`${path.join(luaRocksInstallPath, "bin", "luarocks")} path --lr-bin`, undefined, {
     listeners: {
       stdout: (data) => {
-        lrPath += data.toString()
+        lrPath += data.toString();
       }
     }
-  })
+  });
 
   if (lrPath != "") {
     core.addPath(lrPath.trim());
   }
 
-  let luaPath = ""
+  let luaPath = "";
 
   await exec.exec("luarocks path --lr-path", undefined, {
     listeners: {
       stdout: (data) => {
-        luaPath += data.toString()
+        luaPath += data.toString();
       }
     }
-  })
+  });
 
-  luaPath = luaPath.trim()
+  luaPath = luaPath.trim();
 
-  let luaCpath = ""
+  let luaCpath = "";
 
   await exec.exec("luarocks path --lr-cpath", undefined, {
     listeners: {
       stdout: (data) => {
-        luaCpath += data.toString()
+        luaCpath += data.toString();
       }
     }
-  })
+  });
 
-  luaCpath = luaCpath.trim()
+  luaCpath = luaCpath.trim();
 
   if (luaPath != "") {
-    core.exportVariable("LUA_PATH", ";;" + luaPath)
+    core.exportVariable("LUA_PATH", ";;" + luaPath);
   }
 
   if (luaCpath != "") {
-    core.exportVariable("LUA_CPATH", ";;" + luaCpath)
+    core.exportVariable("LUA_CPATH", ";;" + luaCpath);
   }
 }
 
